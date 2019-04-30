67ae6e389d3d1d7fb9f185ef80144cb741141fe8
kazu69 <kazu69web@gmail.com>
2019-05-01 02:47:32 +0900

    check spec

---
+++ typescript/type-checker/transpile-single-file.ts
@@ -0,0 +1,21 @@
+import * as ts from 'typescript';
+const content = 'import {f} from "foo"\n' + "export var x = f()";
+
+var compilerOptions = { module: ts.ModuleKind.System };
+
+var res1 = ts.transpileModule(content, {
+  compilerOptions: compilerOptions,
+  moduleName: 'myModule2',
+});
+
+console.log(res1.outputText);
+console.log("============");
+
+var res2 = ts.transpile(
+  content,
+  compilerOptions,
+  /*fileName*/ undefined,
+  /*diagnostics*/ undefined,
+  /*moduleName*/ "myModule1"
+);
+console.log(res2);
